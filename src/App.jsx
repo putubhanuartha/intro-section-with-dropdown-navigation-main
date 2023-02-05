0e500570667c178dc768c1ba40fbaa50f1d32a48
Putu Gde Arya Bhanuartha <tuaryabhanu03@gmail.com>
2023-02-05 12:46:15 +0700

    add version 1.0 commecnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { createContext, useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
+// Version 1.0
 export const AppContext = createContext();
 function App() {
-  // Global hooks
-  const [isCollapse, setIsCollapse] = useState(false);
-  return (
-    <AppContext.Provider value={{ isCollapse, setIsCollapse }}>
-      <div className={isCollapse && "overlay"}></div>
-      <Header />
-      <Hero />
-    </AppContext.Provider>
-  );
+	// Global hooks
+	const [isCollapse, setIsCollapse] = useState(false);
+	return (
+		<AppContext.Provider value={{ isCollapse, setIsCollapse }}>
+			<div className={isCollapse && "overlay"}></div>
+			<Header />
+			<Hero />
+		</AppContext.Provider>
+	);
 }
 
 export default App;
